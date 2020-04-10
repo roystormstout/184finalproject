77f8971cd3c161132e057bebc26768b4ce509d57
Lingfeng Guo <lig012@ucsd.edu>
2020-04-09 20:33:23 -0700

    Updates

---
+++ precache-manifest.e571930e196e435ca03414a12213dcea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5bd14f085807180bdd4ad4d001df5b",
+    "url": "/184finalproject/index.html"
+  },
+  {
+    "revision": "7cdc064051504c3386d7",
+    "url": "/184finalproject/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0f44685b8c47f760b68e",
+    "url": "/184finalproject/static/js/2.da619911.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/184finalproject/static/js/2.da619911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cdc064051504c3386d7",
+    "url": "/184finalproject/static/js/main.e879c753.chunk.js"
+  },
+  {
+    "revision": "2093b17c7f42107e92e1",
+    "url": "/184finalproject/static/js/runtime-main.a10b481f.js"
+  }
+]);
